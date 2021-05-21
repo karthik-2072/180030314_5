0357417b15e54c9d5ceb7dde417c8369967fbaee
HI <HI@DESKTOP-55B2VTM>
2021-05-21 12:23:38 +0530

    command line

---
+++ Commandline/src/Sum2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sum2 {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String str1=sc.next();
+	String str2=sc.next();
+	System.out.println(str1+" Technologies "+str2);
+	sc.close();
+}
+}
---
+++ Commandline/src/Welcome.java
@@ -0,0 +1,11 @@
+  
+import java.util.*;
+public class Welcome {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String str1=sc.next();
+	
+	System.out.println("Welcome "+str1);
+	sc.close();
+}
+}
